3da3ac048548ea10c79bf1a94797b9cc51090acc
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2022-07-06 05:20:48 -0400

    pictures

---
+++ amazing-fractal.R
@@ -0,0 +1,21 @@
+my_mandelbrot(
+  xmin = .013583554,
+  xmax = .0135835595,
+  nx = 720,
+  ymin = -.655626684,
+  ymax = -.655626680,
+  ny = 720,
+  n=720,
+  showplot = TRUE)
+
+my_mandelbrot(
+  xmin = .013583554,
+  xmax = .0135835595,
+  nx = 720,
+  ymin = -.655626684,
+  ymax = -.655626680,
+  ny = 720,
+  n=720,
+  showplot = TRUE,
+  cols=colorRampPalette(c("#075D59","#8E2594","#340739","#FFCF59"))(11))
+
